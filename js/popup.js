a71db5122494464be42315a5f5f514333203abc7
Ivo Junior <shymanjr2019@gmail.com>
2025-01-26 14:14:02 -0300

    filtragem por idade e inputs inválidos

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,38 @@
 const resultadoDiv = document.getElementById('result');
 
+function isEmpty(str) {
+    return !str.trim().length;
+}
+
+function calculoPelaIdade(idade, peso) {
+    if (isEmpty(idade)) {
+        return 0;
+    }
+    if (0 < idade <= 17) {
+        return (peso * 40)/1000;
+    }
+    if (18 <= idade < 55) {
+        return (peso * 35)/1000;
+    }
+    if (55 <= idade <= 65) {
+        return (peso * 30)/1000;
+    }
+    if (idade >= 66) {
+        return (peso * 25)/1000;
+    }
+}
+
 function changeResult(input) {
-    if (input === 0) {
-        return 'Valores nulos são inválidos (peso <= 0)';
+    if (!input) {
+        return '';
     }
     return `${input.toFixed(1)} litros por dia`; // Arredondando para 1 casa decimal 
 }
 
 function calculateWeight() {
     const peso = document.getElementById('kg').value;
-    const quantidadeAgua = (peso * 35)/1000 // Transformar de ml para l
+    const idade = document.getElementById('age').value;
+    const quantidadeAgua = calculoPelaIdade(idade, peso);
     
     resultadoDiv.textContent = changeResult(quantidadeAgua);
 }
